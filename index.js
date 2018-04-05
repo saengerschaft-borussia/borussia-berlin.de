bf8f4a403d48c452f1c23fc599c7b59881ec1704
Daan Middendorp <github-d@landgenoot.com>
2018-04-05 09:22:30 +0200

    Band!

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const Koa = require('koa')
 const router = require('koa-router')()
 const request = require('request-promise')
 const app = new Koa()
+const token = 'EAACEdEose0cBAGd1UOL9RG7ImsU3NgGQPbxvOdRpluJ7HJNfWk3x39wvT7pPbrZBN448tO2XFL2qcxVgVGZCfZCmtbENEASx4RlOoDncUkYWiV8DZBIZAbsSLoRMaGj7Xv9iucAPH5ZBgiElPCVGlP44f6NGaQssv4ZAYYmoMy49itt0BeqZBOZADKv3KpZAmlTMC83awjMFJHpgZDZD'
+
 router.get('/api/events', async ctx => {
   let events = JSON.parse(await request(`https://graph.facebook.com/v2.12/Saengerschaftborussia/events/?access_token=${token}&debug=all&format=json&method=get&pretty=0&suppress_http_code=1`))
   let eventsWithPicture = await Promise.all(
